362f2322c9b0ba057b53d99177e60234749e760e
Diki Muhamad Alfikri <dikimuhamad525@gmail.com>
2025-11-07 13:39:37 +0700

    First Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,10 @@ const ctx = document.getElementById('myChart');
 new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Tidak/Belum Sekolah', 'Belum Tamat SD/MI', 'Tamat SD/MI', 'SMP/MTs', 'SMA/SMK', 'D1 & D2', 'D3', 'S1', 'S2', 'S3'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [24.3, 10.65, 22.27, 14.45, 21.51, 0.38, 1.29, 4.78, 0.34, 0.02],
             borderWidth: 1
         }]
     },
